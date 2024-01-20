d749413adaf03dfbe0c8a1ad5a4845e7e2083bea
Anis-leulmi <leulmi_anis@hotmail.com>
2024-01-19 22:45:51 -0500

    Ajouter a la ligne 14 un texte : "Ceci est un deuxième test !"

--- src/main/java/com/example/tp_18/Tp18Application.java
+++ src/main/java/com/example/tp_18/Tp18Application.java
@@ -1,6 +1,6 @@
 package com.example.tp_18;
 
-import jakarta.servlet.ServletOutputStream;
+//import jakarta.servlet.ServletOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +11,7 @@ public class Tp18Application {
         SpringApplication.run(Tp18Application.class, args);
 
         System.out.println("Ceci est un test !");
+        System.out.println("Ceci est un deuxième test !");
     }
 
 }
