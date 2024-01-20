163e1c3faf491f64423d2742c7df03762f8c17a5
Anis-leulmi <leulmi_anis@hotmail.com>
2024-01-19 22:41:47 -0500

    Ajouter a la ligne 13 un text "Ceci est un test !"

--- src/main/java/com/example/tp_18/Tp18Application.java
+++ src/main/java/com/example/tp_18/Tp18Application.java
@@ -1,5 +1,6 @@
 package com.example.tp_18;
 
+import jakarta.servlet.ServletOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class Tp18Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Tp18Application.class, args);
+
+        System.out.println("Ceci est un test !");
     }
 
 }
